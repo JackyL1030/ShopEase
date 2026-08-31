b4b82d8a6d982efbf5143f2877b4b40a8321fd07
Jacky <jackylai1030@gmail.com>
2026-08-30 20:26:52 -0400

    feat: implement routing with Home, Auth, and Checkout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return <div className="app">
+    <Routers>
+      <Route path="/" />
+      <Route path="/auth" />
+      <Route path="/checkout" />
+    </Routers>
+  </div>;
 }
 
 export default App;
